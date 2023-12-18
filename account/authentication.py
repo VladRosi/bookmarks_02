ad519a83ae9671422a684317dc6838dcdac04219
VladRosi <a3714152241a@gmail.com>
2023-12-19 02:05:53 +0300

    First commit

---
+++ account/authentication.py
@@ -0,0 +1,28 @@
+from django.contrib.auth.models import User
+from .models import Profile
+
+
+def create_profile(backend, user, *args, **kwargs):
+  """
+  Create User's profile for the social authentication
+  """
+  Profile.objects.get_or_create(user=user)
+
+class EmailAuthBackend:
+  """
+  Authentication using an email address.
+  """
+  def authenticate(self, request, username=None, password=None):
+    try:
+      user = User.objects.get(email=username)
+      if user.check_password(password):
+        return user
+      return None
+    except (User.DoesNotExist, User.MultipleObjectsReturned):
+      return None
+
+  def get_user(self, user_id):
+    try:
+      return User.objects.get(pk=user_id)
+    except User.DoesNotExist:
+      return None
